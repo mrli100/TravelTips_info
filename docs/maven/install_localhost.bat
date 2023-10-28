4e35e69989af734aca392a3636c332160f4fbd7e
libang <158431766@qq.com>
2023-10-29 00:51:16 +0800

    命令脚本修改

--- docs/maven/install_localhost.bat
+++ docs/maven/install_localhost.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc -Dversion=11.2.0.3 -Dpackaging=jar -Dfile=ojdbc6-11.2.0.3.jar
+mvn install:install-file -DgroupId=com.oracle -DartifactId=ojdbc6 -Dversion=11.2.0.3 -Dpackaging=jar -Dfile=ojdbc6-11.2.0.3.jar
