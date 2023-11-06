813053184a42b95d8b692be4dc7702a3079e44c5
mrlib <gm2010.a>
2023-11-06 11:34:16 +0800

    代码修改

--- UI/client/src/layout/components/client/tabbar/index.vue
+++ UI/client/src/layout/components/client/tabbar/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<van-tabbar v-model="active" route>
 		<van-tabbar-item replace to="/client" icon="wap-home-o">首页</van-tabbar-item>
-		<van-tabbar-item icon="search">圈子</van-tabbar-item>
 		<van-tabbar-item replace to="/team" icon="friends-o">组队</van-tabbar-item>
-		<van-tabbar-item replace to="/travelrouter" icon="location-o">路线</van-tabbar-item>
+		<van-tabbar-item replace to="/travelrouter" icon="location-o">攻略</van-tabbar-item>
+		<van-tabbar-item icon="chat-o">消息</van-tabbar-item>
 		<van-tabbar-item icon="manager-o">我的</van-tabbar-item>
 	</van-tabbar>
 </template>
