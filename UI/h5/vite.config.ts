75a67c02da30a24a9ae0b2a1900d76e80cc0ded4
libang <libang0725@163.com>
2023-11-12 20:41:21 +0800

    初始化h5界面。

---
+++ UI/h5/vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+
+//依赖倒入elementui
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  resolve: {
+    // 配置别名
+    alias: {
+      '@': resolve(__dirname, './src'),
+    }
+  },
+  plugins: [vue(),
+  AutoImport({
+    resolvers: [ElementPlusResolver()],
+  }),
+  Components({
+    resolvers: [ElementPlusResolver()],
+  })],
+  server: {
+    host: '0.0.0.0',
+    port: 3003, // 端口号
+    open: false // 是否自动打开浏览器
+  }
+})
